22ebcaaf35068ad5fb700396deb6d013ad4c986b
Jaelyn Lee <leejay1020@gmail.com>
2024-01-25 23:13:21 +1300

    fix type error

--- src/components/WeightTracker.tsx
+++ src/components/WeightTracker.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import WeightChart from './WeightChart' // Adjust the import path
+// import WeightChart from './WeightChart' // Adjust the import path
 
 interface UserData {
   userWeight: number
   targetWeight: number
 }
 
-const WeightTrackerView: React.FC<UserData> = ({
-  userWeight,
-  targetWeight,
-}) => {
-  return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-6xl">
-        🛠️ Under Construction - Weight Tracker View Page 🛠️
-      </h1>
-    </div>
-  )
-}
+const WeightTrackerView: React.FC<UserData> = () =>
+  //   {
+  //   userWeight,
+  //   targetWeight,
+  // }
+  {
+    return (
+      <div className="flex items-center justify-center">
+        <h1 className="text-6xl">
+          🛠️ Under Construction - Weight Tracker View Page 🛠️
+        </h1>
+      </div>
+    )
+  }
 
 export default WeightTrackerView
--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
-import Home from '../views/HomeView'
 
 function Root() {
   return (
